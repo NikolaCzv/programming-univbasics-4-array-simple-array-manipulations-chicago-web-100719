c47759e1b77a3834eb0e2c14fa02eb2e0a309a42
Nikola Raicic <nikola.raicic@hotmail.com>
2019-09-25 21:48:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,6 +26,6 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def usinf_insert(array, element)
-  array.insert(4, element)
+def usinf_insert(array, item)
+  array.insert(4, item)
 end
